aec801183332a424eb26ff679e5c9f5432a42ee3
rajjo8026 <ranjitb8026@gmail.com>
2022-08-24 16:05:43 +0530

    one mistake for @GetMapping was resolved.

--- GitPracticeSession/src/main/java/com/csi/controller/ApplicationController.java
+++ GitPracticeSession/src/main/java/com/csi/controller/ApplicationController.java
@@ -1,21 +1,21 @@
 package com.csi.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
 public class ApplicationController {
-    @RequestMapping
+    @GetMapping
     public String helloByYogesh(){
         return "welcome to yogeshwar's repo";
     }
 
-    @RequestMapping
+    @GetMapping("/hello")
     public String helloByRanjit(){
         return "welcome to ranjit's repo";
     }
-
-
+    
 }

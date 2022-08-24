a69fb0b1973d71fb22260f67d802835aa67fb859
jaikdm <jaikdm95@gmail.com>
2022-08-24 16:04:03 +0530

    from kartik

--- GitPracticeSession/src/main/java/com/csi/controller/ApplicationController.java
+++ GitPracticeSession/src/main/java/com/csi/controller/ApplicationController.java
@@ -1,14 +1,20 @@
 package com.csi.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
 public class ApplicationController {
-    @RequestMapping
+    @GetMapping // I have changed from RequestMapping to GetMapping
     public String helloByYogesh(){
         return "welcome to yogeshwar's repo";
+    }
+
+    @GetMapping("/kk")
+    public  String sayChange(){
+        return "I have changed from @RequestMapping to @GetMapping";
     }
 }
